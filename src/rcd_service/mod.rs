57f25968275a8882a18fd58759c0edc90af40d91
Randy Le <randyL04@hey.com>
2022-06-25 07:43:33 -0700

    test config

--- src/rcd_service/mod.rs
+++ src/rcd_service/mod.rs
@@ -1,7 +1,36 @@
+use config::Config;
+use std::collections::HashMap;
+
 pub fn hello() {
     println!("hello rcd_service");
+    read_config();
 }
 
 pub fn start(){
     unimplemented!("not completed yet");
+}
+fn read_config() {
+    let settings = Config::builder()
+    // Add in `./Settings.toml`
+    .add_source(config::File::with_name("src/rcd_service/Settings"))
+    // Add in settings from the environment (with a prefix of APP)
+    // Eg.. `APP_DEBUG=1 ./target/app` would set the `debug` key
+    .add_source(config::Environment::with_prefix("APP"))
+    .build()
+    .unwrap();    
+
+
+let priority_setting = String::from("priority");
+
+let priority_value = settings.get_int(&priority_setting).unwrap();
+
+println!("{:?}", priority_value);
+
+// Print out our settings (as a HashMap)
+println!(
+    "{:?}",
+    settings
+        .try_deserialize::<HashMap<String, String>>()
+        .unwrap())
+
 }
