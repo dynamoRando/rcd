cc25afb3759f14b2b0c65f4aafdd4bbe55672ff8
Randy Le <randyL04@hey.com>
2022-07-04 12:26:28 -0700

    add comment

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("{:?}", service);
     service.start();
 
+    // https://stackoverflow.com/questions/62536566/how-can-i-create-a-tokio-runtime-inside-another-tokio-runtime-without-getting-th
     tokio::task::spawn_blocking(move || {
         service.start_client_service();
     }).await.expect("Task panicked");
