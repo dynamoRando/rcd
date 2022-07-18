c97007f14cd3f1d0eb8643af68f800ee33ab5688
Randy Le <randyL04@hey.com>
2022-07-18 06:30:16 -0700

    add table

--- src/rcd/sqlitedb.rs
+++ src/rcd/sqlitedb.rs
@@ -1,6 +1,12 @@
 #[allow(unused_imports)]
 use rusqlite::{named_params, Connection, Error, Result};
+use rusqlite::types::{Type};
 use std::path::Path;
+use table::Table;
+use log::{info};
+
+#[path = "table.rs"]
+mod table;
 
 pub fn create_database(db_name: &str, cwd: &str) -> Result<Connection, Error> {
     let db_path = Path::new(&cwd).join(&db_name);
@@ -17,10 +23,65 @@ pub fn execute_write(db_name: &str, cwd: &str, cmd: &str) -> usize {
 }
 
 #[allow(dead_code)]
-pub fn execute_read(db_name: &str, cwd: &str, cmd: &str) {
+pub fn execute_read(db_name: &str, cwd: &str, cmd: &str) -> Table {
     let db_path = Path::new(&cwd).join(&db_name);
     let conn = Connection::open(&db_path).unwrap();
-    let mut _statement = conn.prepare(cmd).unwrap();
+    let mut statement = conn.prepare(cmd).unwrap();
+    let total_columns = statement.column_count();
+    let col_names = statement.column_names();
+    let mut table = Table::new();
 
-    unimplemented!("need to figure out how to return a table");
+    let mut curr_idx = 0;
+
+    for name in col_names {
+        let c = table::Column {
+            name: name.to_string(),
+            is_nullable: false,
+            idx: curr_idx,
+        };
+
+        curr_idx = curr_idx + 1;
+
+        info!("adding col {}", c.name);
+
+        table.add_column(c);
+    }
+
+    let mut rows = statement.query([]).unwrap();
+
+    while let Some(row) = rows.next().unwrap() {
+        println!("reading row..");
+        let mut data_row = table::Row::new();
+
+        for i in 0..total_columns {
+            let dt = row.get_ref_unwrap(i).data_type();
+
+            let string_value: String = match dt {
+                Type::Blob => String::from(""),
+                Type::Integer => row.get_ref_unwrap(i).as_i64().unwrap().to_string(),
+                Type::Real => row.get_ref_unwrap(i).as_f64().unwrap().to_string(),
+                Type::Text => row.get_ref_unwrap(i).as_str().unwrap().to_string(),
+                _ => String::from("")
+            };
+
+            let string_value = string_value;
+            let col = table.get_column_by_index(i).unwrap();
+
+            let data_item = table::Data {
+                data_string: string_value,
+                data_byte: Vec::new(),
+            };
+
+            let data_value = table::Value {
+                data: Some(data_item),
+                col: col,
+            };
+
+            data_row.add_value(data_value);
+        }
+
+        table.add_row(data_row);
+    }
+
+    return table;
 }
---
+++ src/rcd/table.rs
@@ -0,0 +1,105 @@
+#[allow(dead_code)]
+pub struct Data {
+    pub data_string: String,
+    pub data_byte: Vec<u8>
+}
+
+#[allow(dead_code)]
+pub struct Value {
+    pub data: Option<Data>,
+    pub col: Column
+}
+
+impl Value {
+    #[allow(dead_code)]
+    pub fn is_null(&self) -> bool {
+        return self.data.is_none()
+    }
+}
+
+#[allow(dead_code)]
+pub struct Row {
+    pub vals: Vec<Value>
+}
+
+impl Row {
+    #[allow(dead_code)]
+    pub fn new() -> Self {
+        Self {
+            vals: Vec::new()
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn add_value(&mut self, value: Value){
+        self.vals.push(value);
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Clone)]
+pub struct Column {
+    pub name: String,
+    pub is_nullable: bool,
+    pub idx: usize
+}
+
+#[allow(dead_code)]
+pub struct Table {
+    pub num_cols: usize,
+    pub name: String,
+    pub cols: Vec<Column>,
+    pub rows: Vec<Row>
+}
+
+impl Table {
+    pub fn new() -> Self {
+        Self {
+            num_cols: 0,
+            name: String::from(""),
+            cols: Vec::new(),
+            rows: Vec::new(),
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn set_name(&mut self, name: &str){
+        self.name = name.to_string();
+    }
+
+    #[allow(dead_code)]
+    pub fn set_num_cols(&mut self, total_cols: usize){
+        self.num_cols = total_cols;
+    }
+
+    #[allow(dead_code)]
+    pub fn num_cols(&self) -> usize {
+        return self.num_cols;
+    }
+
+    #[allow(dead_code)]
+    pub fn add_column(&mut self, column: Column) {
+        let current_num_columns = self.cols.len();
+
+        if current_num_columns > self.num_cols {
+            panic!("adding a column exceeds table capacity");
+        }
+
+        self.cols.push(column);
+    }
+
+    #[allow(dead_code)]
+    pub fn add_row(&mut self, row: Row){
+        self.rows.push(row);
+    }
+
+    #[allow(dead_code)]
+    pub fn get_column_by_index(&self, idx: usize) -> Option<Column> {
+        for col in &self.cols {
+            if col.idx == idx {
+                return Some(col.clone());
+            }
+        }
+        return None;
+    }
+}
