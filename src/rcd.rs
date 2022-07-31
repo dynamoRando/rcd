62e208f63b695a130be0b6f742a3a59416802eb5
Randy Le <randyL04@hey.com>
2022-07-31 08:51:57 -0700

    scaffold tests

--- src/rcd.rs
+++ src/rcd.rs
@@ -204,6 +204,37 @@ pub fn get_service_from_config_file() -> RcdService {
 }
 
 pub mod tests {
+    pub mod db_serv {
+        // place_holder
+        pub mod save_contract {
+
+            #[cfg(test)]
+            #[tokio::main]
+            #[allow(dead_code, unused_variables)]
+            async fn client_host(addr_port: &str) {
+                unimplemented!();
+            }
+
+            #[cfg(test)]
+            #[tokio::main]
+            #[allow(dead_code, unused_variables)]
+            async fn client_participant(addr_port: &str) {
+                unimplemented!();
+            }
+
+            #[test]
+            #[allow(dead_code, unused_variables)]
+            fn test() {
+                /*
+                    We will need to kick off two services, the host and the participant
+                    and we will need to also kick off two clients, one for each
+                */
+
+                unimplemented!();
+            }
+        }
+    }
+
     pub mod client_serv {
         pub mod is_online {
             #[cfg(test)]
@@ -822,6 +853,7 @@ pub mod tests {
             use crate::cdata::CreateUserDatabaseRequest;
             #[cfg(test)]
             use crate::rcd;
+            #[cfg(test)]
             use crate::rcd_enum::LogicalStoragePolicy;
             #[cfg(test)]
             use log::info;
