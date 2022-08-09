fe58835208100bce98e47871b79bbe1a33c7b37f
Randy Le <randyL04@hey.com>
2022-08-09 06:55:15 -0700

    add get host_info

--- src/host_info.rs
+++ src/host_info.rs
@@ -1,3 +1,5 @@
+use rusqlite::{Connection, Result};
+
 /*
 "CREATE TABLE IF NOT EXISTS CDS_HOST_INFO
          (
@@ -6,8 +8,54 @@
              TOKEN BLOB NOT NULL
          );",
  */
-pub struct HostInfo{
+
+#[derive(Clone)]
+pub struct HostInfo {
     pub id: String,
     pub name: String,
-    pub token: Vec<u8>
+    pub token: Vec<u8>,
+}
+
+impl HostInfo {
+    pub fn get(conn: Connection) -> HostInfo {
+        let cmd = String::from(
+            "
+        SELECT 
+            HOST_ID, 
+            HOST_NAME, 
+            TOKEN 
+        FROM 
+            CDS_HOST_INFO;",
+        );
+
+        let row_to_host_info =
+            |host_id: String, host_name: String, token: String| -> Result<HostInfo> {
+                let host = HostInfo {
+                    id: host_id,
+                    name: host_name,
+                    token: token.as_bytes().to_vec(),
+                };
+
+                Ok(host)
+            };
+
+        let mut results: Vec<HostInfo> = Vec::new();
+
+        let mut statement = conn.prepare(&cmd).unwrap();
+        let host_infos = statement
+            .query_and_then([], |row| {
+                row_to_host_info(
+                    row.get(0).unwrap(),
+                    row.get(1).unwrap(),
+                    row.get(2).unwrap(),
+                )
+            })
+            .unwrap();
+
+        for hi in host_infos {
+            results.push(hi.unwrap());
+        }
+
+        return results.first().unwrap().clone();
+    }
 }
