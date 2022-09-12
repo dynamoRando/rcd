ddfaab26c9f1c9dbf4334b57223c9a81d0d20ec0
Randy Le <randyL04@hey.com>
2022-09-11 19:10:02 -0700

    Wip (#26)
    * Dev (#24)
    
    * combine from weekend (#15)
    
    weekend work
    
    * combine from weekend (#17)
    
    weekend work
    
    * Wip (#18)
    
    * test
    
    * wip
    
    * wip
    
    * wip
    
    * copyright
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * add gen host info as sqlite method
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * determine sql type
    
    * add parse table name
    
    * wip
    
    * doc
    
    * wip
    
    * wip
    
    * notes
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * Wip (#19)
    
    * test
    
    * wip
    
    * wip
    
    * wip
    
    * copyright
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * add gen host info as sqlite method
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * determine sql type
    
    * add parse table name
    
    * wip
    
    * doc
    
    * wip
    
    * wip
    
    * notes
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * Wip (#20)
    
    * test
    
    * wip
    
    * wip
    
    * wip
    
    * copyright
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * add gen host info as sqlite method
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * determine sql type
    
    * add parse table name
    
    * wip
    
    * doc
    
    * wip
    
    * wip
    
    * notes
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * add to ignore
    
    * Dev (#21) (#23)
    
    * combine from weekend
    
    weekend work
    
    * Wip (#18)
    
    * test
    
    * wip
    
    * wip
    
    * wip
    
    * copyright
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * add gen host info as sqlite method
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * determine sql type
    
    * add parse table name
    
    * wip
    
    * doc
    
    * wip
    
    * wip
    
    * notes
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * Wip (#19)
    
    * test
    
    * wip
    
    * wip
    
    * wip
    
    * copyright
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * add gen host info as sqlite method
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * determine sql type
    
    * add parse table name
    
    * wip
    
    * doc
    
    * wip
    
    * wip
    
    * notes
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * Wip (#20)
    
    * test
    
    * wip
    
    * wip
    
    * wip
    
    * copyright
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * add gen host info as sqlite method
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * determine sql type
    
    * add parse table name
    
    * wip
    
    * doc
    
    * wip
    
    * wip
    
    * notes
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * add to ignore
    
    * fix
    
    * Revert "fix"
    
    This reverts commit 486367051ab6a8039e33c86a8f84ed9e2ba85e56.
    
    * fix db_part

--- src/dbi/sqlite/db_part.rs
+++ src/dbi/sqlite/db_part.rs
@@ -194,12 +194,8 @@ fn create_table_from_schema(table_schema: &TableSchema, conn: &Connection) {
             );
         }
 
- }
-
+        cmd = cmd + &col_statement;
+    }
     cmd = cmd + " ) ";
-
-    // println!("{}", cmd);
-    // println!("{:?}", conn);
-
     execute_write(conn, &cmd);
 }
