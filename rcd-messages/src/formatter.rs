ad7376a609a4829bdad22680b58d42a5e482fcaa
Randy Le <randyL04@hey.com>
2022-11-15 06:37:09 -0700

    placeholders

---
+++ rcd-messages/src/formatter.rs
@@ -0,0 +1,13 @@
+use crate::client::Row;
+
+/// takes a Vec of rows and formats a table similar to MySQL
+#[allow(dead_code, unused_variables)]
+pub fn rows_to_string_table(rows: Vec<Row>) -> String {
+    todo!()
+}
+
+/// takes a Vec of rows and formats a table similiar to Markdown
+#[allow(dead_code, unused_variables)]
+pub fn rows_to_string_markdown_table(rows: Vec<Row>) -> String {
+    todo!()
+}
