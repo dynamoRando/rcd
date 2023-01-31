a408db4c37e6f01759d971549325ecac746a7be7
Randy Le <randyL04@hey.com>
2023-01-30 17:18:07 -0700

    wip

--- rcd-client/tests/participant_tests-delete/delete_from_host_queue.rs
+++ rcd-client/tests/participant_tests-delete/delete_from_host_queue.rs
@@ -18,8 +18,7 @@ pub mod grpc {
         let custom_contract_description = String::from("insert read remote row");
 
         let delete_statement = "DELETE FROM EMPLOYEE WHERE Id = 999";
-        let delete_statement2 = delete_statement.clone();
-
+        
         let where_clause = "Id = 999";
 
         let (tx_main, rx_main) = mpsc::channel();
@@ -141,7 +140,7 @@ pub mod grpc {
             let res = participant_get_and_approve_pending_deletion(
                 &main_db_name_,
                 addr_1_1clone,
-                delete_statement2,
+                delete_statement,
             );
             tx_p_accept_delete.send(res).unwrap();
         })
