364c1a010d7306d6bea08383e9899c39344ef192
Randy Le <randyL04@hey.com>
2023-02-06 20:28:05 -0700

    wip

---
+++ rcd-my-info-core/src/rcd_docker.rs
@@ -0,0 +1,27 @@
+
+#[derive(Debug)]
+pub struct RcdDocker {
+    docker_ip: String
+}
+
+impl RcdDocker {
+    pub fn new(ip_addr_port: String) -> Self {
+        RcdDocker {
+            docker_ip: ip_addr_port
+        }
+    }
+
+    pub async fn list_docker_images(&self) {
+        let docker = docker_api::Docker::new(&self.docker_ip).unwrap();
+    
+        match docker.images().list(&Default::default()).await {
+            Ok(images) => {
+                for image in images {
+                    println!("{:?}", image.repo_tags);
+                }
+            }
+            Err(e) => eprintln!("Something bad happened! {}", e),
+        }
+    }
+    
+}
