d6016628d859ae39dc2cf5b97722363353cdab93
Randy Le <randyL04@hey.com>
2023-02-07 19:24:33 -0700

    wip

--- rcd-foo/src/main.rs
+++ rcd-foo/src/main.rs
@@ -1,19 +1,36 @@
-use log::{debug, error, info, trace, warn, LevelFilter};
-use rcd_sqlite_log::SqliteLog;
+use std::thread;
+
+// use log::{debug, error, info, trace, warn, LevelFilter};
+use rcd_my_info_core::rcd_docker::RcdDocker;
+// use rcd_sqlite_log::SqliteLog;
 
 fn main() {
-    SqliteLog::init(LevelFilter::Trace).unwrap();
+    // SqliteLog::init(LevelFilter::Trace).unwrap();
 
-    debug!("this is a debug");
-    warn!("this is a warn");
-    info!("this is an info");
-    error!("this is an error");
-    trace!("this is a trace");
+    // debug!("this is a debug");
+    // warn!("this is a warn");
+    // info!("this is an info");
+    // error!("this is an error");
+    // trace!("this is a trace");
 
-    let entries = SqliteLog::default_get_last_x_logs(10);
+    // let entries = SqliteLog::default_get_last_x_logs(10);
 
-    for e in &entries {
-        let message = format!("{} {} {} {}", e.dt, e.dt_utc, e.level, e.message);
-        println!("{message}");
-    }
+    // for e in &entries {
+    //     let message = format!("{} {} {} {}", e.dt, e.dt_utc, e.level, e.message);
+    //     println!("{message}");
+    // }
+
+    thread::spawn(move || {
+        let _ = docker_up();
+    })
+    .join()
+    .unwrap();
+}
+
+#[tokio::main]
+async fn docker_up() {
+    println!("connecting");
+    let docker = RcdDocker::new("tcp://127.0.0.1:2375".to_string());
+    docker.list_docker_containers().await;
+    docker.list_docker_images().await;
 }
