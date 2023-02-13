7797f7dbbc7db5c417b94d7d108d5bd2703189a4
Randy Le <randyL04@hey.com>
2023-02-13 07:22:55 -0700

    wip

--- rcd-my-info-core/src/admin.rs
+++ rcd-my-info-core/src/admin.rs
@@ -1,28 +1,44 @@
-use std::{path::Path, env};
+use std::{path::Path};
 
-use crate::admin_db::DbType;
+use crate::{admin_db::DbType, admin_db_sqlite::SqliteDb};
 
+#[allow(dead_code)]
 pub struct Admin {
-    pub db_type: DbType
+    db_type: DbType,
+    root_dir: String,
+    sqlite: Option<SqliteDb>
 }
 
 impl Admin {
-    pub fn new(db_type: DbType) -> Self {
-        Admin {
-            db_type
+    pub fn new(db_type: DbType, root_dir: String) -> Self {
+        match db_type {
+            DbType::Sqlite => {
+                let path = Path::new(&root_dir).join("my_info.db");
+                let db = SqliteDb::new(path.to_str().unwrap().to_string());
+                let admin =  Admin { db_type: DbType::Sqlite, root_dir, sqlite: Some(db) };
+                return admin;
+
+            },
+            DbType::MySql => todo!(),
+            DbType::Postgres => todo!(),
         }
     }
 
-    #[allow(unused_variables)]
-    pub fn register_user(&self, un: &str, pw: &str) -> Result<bool, String> {
-        todo!()
+    pub fn register_user(&self, email: &str, pw: &str) -> Result<bool, String> {
+        match self.db_type {
+            DbType::Sqlite => {
+                self.sqlite.as_ref().unwrap().save_account(email, pw)
+            },
+            DbType::MySql => todo!(),
+            DbType::Postgres => todo!(),
+        }
     }
 
     #[allow(dead_code)]
-    fn get_sqlite_path() -> String {
-        let cwd = env::current_dir().unwrap();
-        let db_path = Path::new(&cwd).join("my_info.db");
-        let x = db_path.as_os_str().to_str().unwrap();
-        x.to_string()
+    fn get_sqlite_path(&self) -> String {
+        // let cwd = env::current_dir().unwrap();
+        let db_path = Path::new(&self.root_dir).join("my_info.db");
+        let path = db_path.as_os_str().to_str().unwrap();
+        path.to_string()
     }
 }
