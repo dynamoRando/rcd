77d133def91e6eb0fe9c7c8f039bf1430cad7d5f
Randy Le <randyL04@hey.com>
2023-03-25 14:16:40 -0700

    remove

--- rcd-foo/build.rs
+++
@@ -1,9 +0,0 @@
-use std::io;
-
-fn main() -> io::Result<()> {
-    tonic_build::configure()
-        .build_server(false)
-        .build_client(true)
-        .out_dir("./src")
-        .compile(&["rcdp.proto"], &["."])
-}
