7f391d477c4662181655c8c9b42bdac357f91b3d
Randy Le <randyL04@hey.com>
2023-04-01 10:19:10 -0700

    wip

--- tracking/src/repo.rs
+++ tracking/src/repo.rs
@@ -1,3 +1,4 @@
+use chrono::NaiveDateTime;
 use rcd_messages::{
     client::{ExecuteReadReply, ExecuteReadRequest},
     proxy::request_type::RequestType,
@@ -48,36 +49,59 @@ impl Repo {
 
         let read_request_json = serde_json::to_string(&request).unwrap();
 
-        let callback_associated_events = Callback::from(move |response: Result<AttrValue, String>| {
-            if let Ok(ref x) = response {
-                log_to_console(x);
+        let callback_associated_events =
+            Callback::from(move |response: Result<AttrValue, String>| {
+                if let Ok(ref x) = response {
+                    log_to_console(x);
 
-                let read_reply: ExecuteReadReply = serde_json::from_str(x).unwrap();
+                    let read_reply: ExecuteReadReply = serde_json::from_str(x).unwrap();
 
-                let is_authenticated = read_reply
-                    .authentication_result
-                    .as_ref()
-                    .unwrap()
-                    .is_authenticated;
+                    let is_authenticated = read_reply
+                        .authentication_result
+                        .as_ref()
+                        .unwrap()
+                        .is_authenticated;
 
-                if is_authenticated {
-                    let result = read_reply.results.first().unwrap();
-                    if !result.is_error {
-                        let rows = result.clone().rows;
+                    if is_authenticated {
+                        let result = read_reply.results.first().unwrap();
+                        if !result.is_error {
+                            let rows = result.clone().rows;
 
-                        for row in &rows {
-                            
+                            for row in &rows {
+                                let event_id: u32;
+                                let event_type: u32;
+                                let event_date: NaiveDateTime;
+                                let notes: String;
+
+                                for value in row.values {
+                                    if let Some(column) = value.column {
+                                        if column.column_name == "event_id" {
+                                            let result_event_id = value.string_value.parse::<u32>();
+                                            if let Ok(event_id) = result_event_id {
+                                                event_id = event_id;
+                                            } else {
+                                                event_id = 0;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+                            todo!()
                         }
-
-                        todo!()
+                    } else {
+                        log_to_console("warning: we are not logged in to rcd");
                     }
+                } else {
+                    log_to_console("warning: we are not logged in to proxy");
                 }
-            } else {
-                log_to_console("warning: we are not logged in");
-            }
-        });
+            });
 
-        request::post(RequestType::ReadAtHost, &read_request_json, callback_associated_events);
+        request::post(
+            RequestType::ReadAtHost,
+            &read_request_json,
+            callback_associated_events,
+        );
 
         todo!()
     }
