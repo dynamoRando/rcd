2f1229887500e8f1bf5a47042d444fc05ebddf8f
Randy Le <randyL04@hey.com>
2023-04-06 07:33:28 -0700

    wip

--- tracking-api/src/srv/user/create.rs
+++ tracking-api/src/srv/user/create.rs
@@ -1,10 +1,50 @@
 use log::debug;
 use rocket::{http::Status, post, serde::json::Json};
-use tracking_model::user::User;
+use tracking_model::user::{CreateUserResult, User};
+
+use crate::srv::{get_client, shark_event::get::DB_NAME};
 
 #[post("/user/create", format = "application/json", data = "<request>")]
-pub async fn create_account(request: Json<User>) -> Status {
+pub async fn create_account(request: Json<User>) -> (Status, Json<CreateUserResult>) {
     debug!("{request:?}");
 
-    todo!()
+    let u = request.into_inner();
+    let has_account = has_account_with_name(&u.un).await;
+    if !has_account {}
+
+    let result = CreateUserResult {
+        is_successful: false,
+        message: None,
+    };
+
+    return (Status::Ok, Json(result));
+}
+
+async fn has_account_with_name(un: &str) -> bool {
+    let mut client = get_client().await;
+
+    let sql = "SELECT COUNT(*) cnt FROM user_to_participant WHERE un = ':un'";
+    let sql = sql.replace(":un", un);
+
+    let result = client.execute_read_at_host(DB_NAME, &sql, 1).await.unwrap();
+
+    if !result.is_error {
+        let rows = result.clone().rows;
+        for row in &rows {
+            for value in &row.values {
+                if let Some(column) = &value.column {
+                    if column.column_name == "cnt" {
+                        let rv = value.string_value.parse::<u32>();
+                        if let Ok(v) = rv {
+                            return v > 0;
+                        } else {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    true
 }
--- tracking-model/src/user.rs
+++ tracking-model/src/user.rs
@@ -7,3 +7,10 @@ pub struct User {
     pub alias: Option<String>,
     pub id: Option<String>,
 }
+
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+pub struct CreateUserResult {
+    pub is_successful: bool,
+    pub message: Option<String>,
+}
