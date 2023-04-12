721de7a9e2af8bf71d47ef08977c1668df4c0d36
Randy Le <randyL04@hey.com>
2023-04-12 07:11:11 -0700

    w

--- tracking/src/pages/events/mod.rs
+++ tracking/src/pages/events/mod.rs
@@ -2,14 +2,15 @@ use yew::prelude::*;
 
 use crate::pages::events::{
     add::{AddAssociatedEvent, AddMainEvent},
-    view::ViewEvents, stats::Stats,
+    stats::Stats,
+    view::ViewEvents,
 };
 
 pub mod add;
 pub mod stats;
 pub mod view;
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Copy)]
 pub enum CurrentEventPage {
     Default,
     AddMainEvent,
@@ -32,33 +33,73 @@ pub fn Events() -> Html {
         CurrentEventPage::Stats => html! {<div><Stats /></div>},
     };
 
+    let onclick_add_main = {
+        let page_state = page_state.clone();
+        Callback::from(move |_| {
+            let page_state = page_state.clone();
+            page_state.set(CurrentEventPage::AddMainEvent)
+        })
+    };
+
+    let onclick_add_associated = {
+        let page_state = page_state.clone();
+        Callback::from(move |_| {
+            let page_state = page_state.clone();
+            page_state.set(CurrentEventPage::AddAssociatedEvent)
+        })
+    };
+
+    let onclick_view = {
+        let page_state = page_state.clone();
+        Callback::from(move |_| {
+            let page_state = page_state.clone();
+            page_state.set(CurrentEventPage::ViewEvents)
+        })
+    };
+
+    let onclick_stats = {
+        let page_state = page_state.clone();
+        Callback::from(move |_| {
+            let page_state = page_state.clone();
+            page_state.set(CurrentEventPage::Stats)
+        })
+    };
+
+    let onclick_default = {
+        let page_state = page_state.clone();
+        Callback::from(move |_| {
+            let page_state = page_state.clone();
+            page_state.set(CurrentEventPage::Default)
+        })
+    };
+
     html! {
         <div>
             <div class="container">
                 <div class="box">
 
                     <div class="has-text-centered">
-                        <p><h1 class="title is-1">{ "Events" }</h1></p>
+                        <p><h1 class="title is-1" onclick={onclick_default}>{ "Events" }</h1></p>
 
                         <aside class="menu">
                              <p class="menu-label">
                               {"Add Events"}
                              </p>
                              <ul class="menu-list">
-                                 <li><a>{"Main Events"}</a></li>
-                                 <li><a>{"Associated Events"}</a></li>
+                                 <li onclick={onclick_add_main}><a>{"Main Events"}</a></li>
+                                 <li onclick={onclick_add_associated}><a>{"Associated Events"}</a></li>
                              </ul>
                              <p class="menu-label">
                               {"View Events"}
                              </p>
                              <ul class="menu-list">
-                                 <li><a>{"Events"}</a></li>
+                                 <li onclick={onclick_view}><a>{"Events"}</a></li>
                              </ul>
                              <p class="menu-label">
                              {"Stats"}
                             </p>
                             <ul class="menu-list">
-                                 <li><a>{"View Statistics"}</a></li>
+                                 <li onclick={onclick_stats}><a>{"View Statistics"}</a></li>
                              </ul>
                         </aside>
                     </div>
